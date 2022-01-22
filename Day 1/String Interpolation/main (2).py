16db7a71649e7654474b6d167153da56d3cad4a8
Poonam Wagh <70890325+0604-poonam@users.noreply.github.com>
2022-01-22 20:41:55 +0530

    Add files via upload

---
+++ Day 1/String Interpolation/main (2).py
@@ -0,0 +1,20 @@
+
+#inline arithmetic
+
+a = 25
+b = 10
+
+#addition
+print(f'25 + 10 is {a + b}.')
+
+#subtraction
+print(f'25 - 10 is {a - b}.')
+
+#multiplication
+print(f'25 * 10 is {a * b}.')
+
+#division
+print(f'25 / 10 is {a / b}.')
+
+#floor division
+print(f'25 % 10 is {a % b}.')
