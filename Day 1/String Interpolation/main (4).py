44b836bc222a4cb809a5f859c21bb3bf7493e477
Poonam Wagh <70890325+0604-poonam@users.noreply.github.com>
2022-01-22 21:12:48 +0530

    Add files via upload

---
+++ Day 1/String Interpolation/main (4).py
@@ -0,0 +1,12 @@
+
+#EXAMPLE1
+
+name = 'all'
+print('Hello, {}'.format(name))
+
+#EXAMPLE 2
+
+name = 'all'
+language ='python'
+print('Hello {name}! I am learning {language}.'.format(name=name,language=language))
+
