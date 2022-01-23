0be71ad3e81ce504e11639713fe13eedd3d2efde
Poonam Wagh <70890325+0604-poonam@users.noreply.github.com>
2022-01-23 19:22:32 +0530

    Add files via upload

---
+++ Day 1/Operators/main (15).py
@@ -0,0 +1,19 @@
+
+#identity operator
+
+x1 = 10
+y1 = 20
+x2 = 'hii'
+y2 = 'hii'
+x3 = [10,20,30]
+y3 = [10,20,30]
+
+# Output: True
+print(x1 is not y1)
+
+# Output: True
+print(x2 is y2)
+
+# Output: False
+print(x3 is y3)
+
