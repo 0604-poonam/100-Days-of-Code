82ac4f61b83f810eb1253df4762891827da32a4c
Poonam Wagh <70890325+0604-poonam@users.noreply.github.com>
2022-01-23 19:40:27 +0530

    Add files via upload

---
+++ Day 1/Operators/main (16).py
@@ -0,0 +1,17 @@
+
+#membership operator
+x = 'Hello World'
+y = {1:'a',2:'b'}
+
+# Output: True
+print('W' in x)
+
+# Output: True
+print('hello' not in x)
+
+# Output: True
+print(1 in y)
+
+# Output: False
+print('a' in y)
+
