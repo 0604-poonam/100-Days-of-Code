c41565e579ec8e368e3649ab4e3837c3edc0c604
Poonam Wagh <70890325+0604-poonam@users.noreply.github.com>
2022-01-23 19:12:37 +0530

    Add files via upload

---
+++ Day 1/Operators/main (12).py
@@ -0,0 +1,32 @@
+
+x = 25
+y = 10
+
+#addition
+# Output: x + y = 35
+print('x + y =',x+y)
+
+#subtraction
+# Output: x - y = 15
+print('x - y =',x-y)
+
+#multiplication
+# Output: x * y = 250
+print('x * y =',x*y)
+
+#division
+# Output: x / y = 2.5
+print('x / y =',x/y)
+
+#floor division
+# Output: x // y = 2
+print('x // y =',x//y)
+
+#exponent
+# Output: x ** y = 95367431640625
+print('x ** y =',x**y)
+
+
+
+
+
