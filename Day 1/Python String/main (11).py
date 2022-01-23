138402c8b6d98cf16d4ea1a27b8add1074c0e6b3
Poonam Wagh <70890325+0604-poonam@users.noreply.github.com>
2022-01-23 19:00:57 +0530

    Add files via upload

---
+++ Day 1/Python String/main (11).py
@@ -0,0 +1,18 @@
+
+#single line basestring
+s1 = 'hello world'
+
+print(s1)
+
+#multiline string
+s2 = '''multiline 
+string '''
+     
+print(s2)
+
+print('s1[0]',s1[0])
+
+print('s1[6:11]',s1[6:11])
+
+
+
