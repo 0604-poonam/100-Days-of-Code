bc3e2b03b933bfd42d7ceacae00c8cf3ea5dc7a8
Poonam Wagh <70890325+0604-poonam@users.noreply.github.com>
2022-01-23 18:48:51 +0530

    Add files via upload

---
+++ Day 1/Set/main (9).py
@@ -0,0 +1,15 @@
+
+set1 = {10,20,30,40,50,60,70,80}
+
+# printing set variable
+print("set 1 = ", set1)
+
+# data type of variable a
+print(type(set1))
+
+
+
+set2={5,15,15,20,20,30,62,25,25}
+#Sets have unique values. They eliminate duplicates.
+print("set 2=",set2)
+
