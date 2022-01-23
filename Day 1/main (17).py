bc494303e75faa55bda1bdff85a41f4ef3ea0658
Poonam Wagh <70890325+0604-poonam@users.noreply.github.com>
2022-01-23 20:10:08 +0530

    Add files via upload

---
+++ Day 1/main (17).py
@@ -0,0 +1,20 @@
+
+# Declaring and assigning value to a variable
+
+a = "tom"
+print(a)
+
+#Changing the value of a variable
+a = "jerry"
+print(a)
+
+#Assigning multiple values to multiple variables
+a,b,c = 100,"abc",10.23
+
+print(b)
+
+#Assigning same value to multiple variables
+x = y = z = 'world' 
+
+print(x,y,z)
+
