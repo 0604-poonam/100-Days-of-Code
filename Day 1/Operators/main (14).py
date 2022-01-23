f956b40018585a07fc85c34d4ae805be929b0011
Poonam Wagh <70890325+0604-poonam@users.noreply.github.com>
2022-01-23 19:18:42 +0530

    Add files via upload

---
+++ Day 1/Operators/main (14).py
@@ -0,0 +1,22 @@
+
+#logical operator
+
+x = True
+y = False
+
+print('x and y is',x and y)
+
+print('x or y is',x or y)
+
+print('not x is',not x)
+
+
+a = True
+b = True
+
+print('a and b is',a and b)
+
+print('a or b is',a or b)
+
+print('not b is',not a)
+
