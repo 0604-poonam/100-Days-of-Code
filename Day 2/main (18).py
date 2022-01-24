d53161beba5f786c81933396e156413b85e4b820
Poonam Wagh <70890325+0604-poonam@users.noreply.github.com>
2022-01-24 19:19:12 +0530

    Add files via upload

---
+++ Day 2/main (18).py
@@ -0,0 +1,17 @@
+
+#program to add two numbers
+
+a = 2
+b = 5
+
+sum = a+b 
+print("a+b is",sum)
+
+#Adding Two Numbers With User Input
+
+num1 = input("Enter first number ")
+num2 = input("Enter second number ")
+
+num3 = float(num1)+float(num2)
+
+print("The sum of {0} and {1} is {2}".format(num1,num2,num3))
