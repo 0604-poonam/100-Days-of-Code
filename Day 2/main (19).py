0fd3d70bfd66732fabf3209f252e16263f6ba1eb
Poonam Wagh <70890325+0604-poonam@users.noreply.github.com>
2022-01-24 19:56:40 +0530

    Add files via upload

---
+++ Day 2/main (19).py
@@ -0,0 +1,14 @@
+
+# Python Program to calculate the square root
+
+num1 = 8 
+
+
+num2 = float(input('Enter a number: '))
+
+num_sqrt1 = num1 ** 0.5
+num_sqrt2 = num2 ** 0.5
+
+print('The square root of %0.3f is %0.3f'%(num1 ,num_sqrt1))
+print('The square root of %0.3f is %0.3f'%(num2 ,num_sqrt2))
+
