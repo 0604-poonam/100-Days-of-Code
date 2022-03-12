93e710aeb7abca202906e7c7b3cd55fdde4a3a1c
Poonam Wagh <70890325+0604-poonam@users.noreply.github.com>
2022-03-12 23:55:02 +0530

    Update Hello World! Program.py

--- Day 1/Hello World! Program.py
+++ Day 1/Hello World! Program.py
@@ -2,4 +2,4 @@
 
 print("Hello World")
 
-print("Hello everyone,I'm publicly committing to the 100DaysOfCode Challenge starting today! Learn More and Join me! #100DaysOfCode")
+print("Hello everyone")
